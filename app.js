736aa2eae4a42c79e43183185d02b40b85ef6d11
Leonardo Ramos <Leocreedlrs@gmail.com>
2025-05-20 22:15:37 -0300

    Commit 2 teste
    Fiz algumas implementações

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Seja bem vindo a festa");
+let nome = prompt ("Me fale seu nome");
+let idade = prompt ("Me Fale sua idade");
+
+if(idade < 18){
+    alert (`Você é menor de idade ${nome}, sai daqui, por favor!!!`);
+} else{
+    alert("seja bem-vindo");
+}
